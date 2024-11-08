d8032f0810ac2bcf38fbfd2d8f53aad65b410616
Kuldeep <165891991+AtharvKashiv@users.noreply.github.com>
2024-11-08 19:46:19 +0530

    Login page design completed.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+   <LoginForm/>
   );
 }
 
